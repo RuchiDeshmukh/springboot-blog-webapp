35f41179fbcbd163dcddfbff795b2745916236af
Deshmukh <2238186@cognizant.com>
2024-01-22 20:00:06 +0530

    Feature - Delete Post(Admin)

--- src/main/java/com/blogwebapp/controller/PostController.java
+++ src/main/java/com/blogwebapp/controller/PostController.java
@@ -67,6 +67,12 @@ public class PostController {
 		postService.updatePost(postDto);
 		return "redirect:/admin/posts";
 	}
+	
+	@GetMapping("/admin/posts/{postId}/delete")
+	public String deletePost(@PathVariable("postId") Long postId) {
+		postService.deletePost(postId);
+		return "redirect:/admin/posts";
+	}
 
 	private static String getUrl(String postTitle) {
 		String title = postTitle.trim().toLowerCase();
